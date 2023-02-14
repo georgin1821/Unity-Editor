85f25565c507f5d22bbbd0e989c8fca141ff04d2
Nik Geo <georgiopoulosn@gmail.com>
2023-02-14 23:21:42 +0200

    atrrProp1

---
+++ Assets/Scripts/SeperatorAttribute.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.AttributeUsage(System.AttributeTargets.Field, AllowMultiple = true)]
+public class SeperatorAttribute : PropertyAttribute
+{
+    public readonly float Height;
+    public readonly float Spacing;
+
+    public SeperatorAttribute(float height = 1, float spacing  = 10)
+    {
+        Height = height;
+        Spacing = spacing;
+    }
+}
